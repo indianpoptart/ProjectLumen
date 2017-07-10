5da8f3b9a44c4ab9b7b2f47c048f16f30251f86e
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2017-07-10 03:06:53 -0400

    v0.1
    - Chop Detection is working

--- app/src/main/java/org/nikhilp/projectlumen/activities/MainActivity.java
+++ app/src/main/java/org/nikhilp/projectlumen/activities/MainActivity.java
@@ -1,8 +1,17 @@
-package org.nikhilp.projectlumen;
+package org.nikhilp.projectlumen.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +22,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.nisrulz.sensey.ChopDetector;
+import com.github.nisrulz.sensey.Sensey;
+
+import org.nikhilp.projectlumen.R;
+import org.nikhilp.projectlumen.services.BackgroundService;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +56,26 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!isFlashAvailable) {
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
+            alert.setTitle(getString(R.string.app_name));
+            alert.setMessage("Error");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
+        Intent i = new Intent(this, BackgroundService.class);
+        startService(i);
+        Log.d("ProjectLumen","loaded");
+
     }
+
 
     @Override
     public void onBackPressed() {
@@ -81,7 +116,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -97,5 +132,9 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        Sensey.getInstance().stop();
     }
 }
